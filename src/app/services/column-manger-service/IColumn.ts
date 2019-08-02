8bed34721ace3080a5f4217360839d2909ea9597
Andrey Pretskailo <andrey.pretskailo@intexsoft.by>
2019-08-02 14:05:40 +0300

    add column documentation

--- src/app/services/column-manger-service/IColumn.ts
+++ src/app/services/column-manger-service/IColumn.ts
@@ -1,13 +1,40 @@
 import {ColumnsTypes} from 'app/enums/columnsTypes.enum';
 
 export interface IColumn {
+    /**
+     * Ширина колонки.
+     */
     width: number;
+    /**
+     * Название колонки.
+     */
     title: string;
+    /**
+     * Тип колнки. Может быть STATUS, DATE, TEXT.
+     */
     type: ColumnsTypes;
+    /**
+     * Имя колонки.
+     */
     name: string;
+    /**
+     * Разрешение на изменение ширины колоки
+     */
     resizable: boolean;
+    /**
+     * Разрешение на сортировку.
+     */
     sortable: boolean;
+    /**
+     * Разрешение на перетягивание колонки.
+     */
     draggable: boolean;
+    /**
+     * Разрешение на фильтрацию по колонке.
+     */
     filterable: boolean;
+    /**
+     * Минимальная ширина колонки.
+     */
     minWidth: number;
 }
